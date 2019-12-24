ec7f97c8d339bf01cc9629a42b70ec0f9dc86737
Maciej Zając <maciej.zajac.197@gmail.com>
2019-12-24 22:30:52 +0100

    implementing material ui

--- client/src/components/header/Navbar.js
+++ client/src/components/header/Navbar.js
@@ -1,21 +1,109 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { useSelector, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
 import { SIsAuthenticated } from "../../selectors";
 import { ALogout } from "../../actions/authActions";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import Switch from "@material-ui/core/Switch";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormGroup from "@material-ui/core/FormGroup";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
 
 const Navbar = () => {
+  const classes = useStyles();
   const dispatch = useDispatch();
 
-  const isAuthenticated = useSelector(SIsAuthenticated);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const isAuthenticated = true; //useSelector(SIsAuthenticated);
 
   const handleLogout = () => {
     dispatch(ALogout());
   };
 
+  const handleMenu = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
-    <div>
-      <nav className="nav">
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            <Link to="/">Home</Link>
+          </Typography>
+
+          {isAuthenticated && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right"
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right"
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem
+                  component={Link}
+                  to="/dashboard"
+                  onClick={handleClose}
+                >
+                  Dashboard
+                </MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+      {/* <nav className="nav">
         <div className="nav-container">
           <div>
             <Link to="/" className="nav__logo">
@@ -44,7 +132,7 @@ const Navbar = () => {
             )}
           </div>
         </div>
-      </nav>
+      </nav> */}
     </div>
   );
 };
